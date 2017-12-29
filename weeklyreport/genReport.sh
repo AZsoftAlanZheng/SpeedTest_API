fecec25898a45097f4b6af10dd1dad8867342ee4
alan <a0955857152@gmail.com>
2017-12-29 17:56:38 +0800

    bug fixed

--- weeklyreport/genReport.sh
+++ weeklyreport/genReport.sh
@@ -9,21 +9,21 @@ node $BASEDIR/index.js > $BASEDIR/source.md
 OUT=$?
 if [ $OUT -ne 0 ];then
    echo "node $BASEDIR/index.js > $BASEDIR/source.md failed"
-   return 111
+   exit 111
 fi
 
 node $BASEDIR/md2html.js $BASEDIR/source.md > $BASEDIR/source.html
 OUT=$?
 if [ $OUT -ne 0 ];then
    echo "node $BASEDIR/md2html.js $BASEDIR/source.md > $BASEDIR/source.html failed"
-   return 112
+   exit 112
 fi
 
 node $BASEDIR/html2pdf.js $BASEDIR/source.html $BASEDIR/source.pdf
 OUT=$?
 if [ $OUT -ne 0 ];then
    echo "node $BASEDIR/html2pdf.js $BASEDIR/source.html $BASEDIR/source.pdf failed"
-   return 113
+   exit 113
 fi
 
 echo "done, output file is $BASEDIR/source.pdf"
